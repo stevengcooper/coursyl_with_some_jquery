bc38481f114fc63bb9d7b517cad850834fe827e3
Mason F. Matthews <mason.f.matthews@gmail.com>
2016-03-16 12:44:18 -0400

    Add changes from class.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,109 +16,38 @@
 //= require_tree .
 
 
-function disableSubmitButton(button) {
-  var submitButton = document.getElementById("save");
-  submitButton.disabled = true;
+function disableSubmitButton() {
+  $(event.target).prop("disabled", true);
+  $(event.target).closest("form").submit();
 }
 
+function bindSubmitClick() {
+  $("input[type=submit]").on("click", disableSubmitButton);
+}
 
-function deleteRow(arrayId) {
-  var row = document.getElementsByClassName("btn-danger")[arrayId].parentNode.parentNode;
-  row.style.display = "none";
-  var checkbox = document.getElementsByClassName("destroy")[arrayId];
-  checkbox.value = true;
+$(bindSubmitClick);
 
+
+function deleteRow() {
+  var row = $(event.target).closest(".association");
+  row.css("display", "none");
+  var checkbox = $(event.target).siblings().last();
+  checkbox.prop("checked", true);
 }
 
 
 function displayGrade(displayType) {
-  var associations = document.getElementById("associations");
-  var last_section = associations.lastElementChild;
+  var associations = $("#associations");
+  var last_section = associations.children().last();
   if (displayType) {
-    last_section.style.display = "block";
-    var clicked = true
+    last_section.css("display", "block");
   } else {
-    last_section.style.display = "none";
+    last_section.css("display", "none");
   }
 }
 
+function hideLastRow() {
+  displayGrade(false);
+}
 
-(function() {
-
-     'use strict';
-
-    // Feature Test
-    if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
-
-        // Function to animate the scroll
-        var smoothScroll = function (anchor, duration) {
-
-            // Calculate how far and how fast to scroll
-            var startLocation = window.pageYOffset;
-            var endLocation = anchor.offsetTop;
-            var distance = endLocation - startLocation;
-            var increments = distance/(duration/16);
-            var stopAnimation;
-
-            // Scroll the page by an increment, and check if it's time to stop
-            var animateScroll = function () {
-                window.scrollBy(0, increments);
-                stopAnimation();
-            };
-
-            // If scrolling down
-            if ( increments >= 0 ) {
-                // Stop animation when you reach the anchor OR the bottom of the page
-                stopAnimation = function () {
-                    var travelled = window.pageYOffset;
-                    if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
-                        clearInterval(runAnimation);
-                    }
-                };
-            }
-            // If scrolling up
-            else {
-                // Stop animation when you reach the anchor OR the top of the page
-                stopAnimation = function () {
-                    var travelled = window.pageYOffset;
-                    if ( travelled <= (endLocation || 0) ) {
-                        clearInterval(runAnimation);
-                    }
-                };
-            }
-
-            // Loop the animation function
-            var runAnimation = setInterval(animateScroll, 16);
-
-        };
-
-        // Define smooth scroll links
-        var scrollToggle = document.querySelectorAll('.scroll');
-
-        // For each smooth scroll link
-        [].forEach.call(scrollToggle, function (toggle) {
-
-            // When the smooth scroll link is clicked
-            toggle.addEventListener('click', function(e) {
-
-                // Prevent the default link behavior
-                e.preventDefault();
-
-                // Get anchor link and calculate distance from the top
-                var dataID = toggle.getAttribute('href');
-                var dataTarget = document.querySelector(dataID);
-                var dataSpeed = toggle.getAttribute('data-speed');
-
-                // If the anchor exists
-                if (dataTarget) {
-                    // Scroll to the anchor
-                    smoothScroll(dataTarget, dataSpeed || 500);
-                }
-
-            }, false);
-
-        });
-
-    }
-
- })();
+$(hideLastRow);
